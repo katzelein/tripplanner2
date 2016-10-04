8936bba3eb6ff89f063a98b9e1dd0941dd2c5d84
Kat Guthrie <kathrynguthrie@gmail.com>
2016-10-03 20:13:27 -0400

    added design stuff

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -5,7 +5,9 @@ function initialize_gmaps() {
     var mapOptions = {
         center: myLatlng,
         zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // If you want to get rid of the stylized map, delete everything in styles:
+        styles: [{"featureType":"water","elementType":"all","stylers":[{"hue":"#1CB2BD"},{"saturation":53},{"lightness":-44},{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"hue":"#1CB2BD"},{"saturation":40}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#BBDC00"},{"saturation":80},{"lightness":-20},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"}]}]
     };
     // get the maps div's HTML obj
     var map_canvas_obj = document.getElementById("map-canvas");
@@ -18,6 +20,13 @@ function initialize_gmaps() {
     });
     // Add the marker to the map by calling setMap()
     marker.setMap(map);
+
+    // Set this up later if there's time -- specific hotel, food & activity-shaped markers
+    // var hotelMarker = new google.maps.Marker({
+    // position: {lat: -33.890, lng: 151.274},
+    // map: map,
+    // icon: image
+  // });
 }
 
 $(document).ready(function() {
